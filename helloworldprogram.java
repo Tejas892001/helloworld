01428f3746247819570b7eec63d062fd41a2e3ec
Tejas892001 <98046210+Tejas892001@users.noreply.github.com>
2023-01-18 17:49:52 +0530

    Update helloworldprogram.java

--- helloworldprogram.java
+++ helloworldprogram.java
@@ -1,5 +1,7 @@
 public class helloworldprogram {
    public static void main(String[] args) {
     System.out.println("Hello World Sample Code.");
+       System.out.println("Hello World Sample Code.");
+      
     }
 }
